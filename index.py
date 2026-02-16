96cfa93e79defea758a6e1af28043e604186b339
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-02-16 20:43:48 +0530

    Refactor salary method with capitalized variables

--- index.py
+++ index.py
@@ -1,35 +1,33 @@
 class Management:
     def salary(self):
-        name = input("Enter your name: ")
-        phone=int(input("enter you phone:"))
-        total_salary = float(input("Enter your monthly salary: "))
-        total_days = 30
-        leave_days = int(input("Enter number of leave days: "))
+        Name = input("Enter your name: ")
+        Phone=int(input("enter you phone:"))
+        Total_salary = float(input("Enter your monthly salary: "))
+        Total_days = 30
+        Leave_days = int(input("Enter number of leave days: "))
 
-        attendance = total_days - leave_days
-        per_day_salary = total_salary / total_days
-        deduction = per_day_salary * leave_days
-        receive_salary = total_salary - deduction
+        Attendance = Total_days - Leave_days
+        Per_day_salary = Total_salary / Total_days
+        Deduction = Per_day_salary * Leave_days
+        Receive_salary = Total_salary - Deduction
 
         print("\n----- Salary Details -----")
-        print("Name:", name)
-        print("phone:", phone)
-        print("Monthly salary:",total_salary)
-        print("Total Attendance:", attendance)
-        print("Leave Deduction:", deduction)
-        print("Received Salary:", receive_salary)
-
-
-
+        print("Name:", Name)
+        print("phone:", Phone)
+        print("Monthly salary:",Total_salary)
+        print("Total Attendance:", Attendance)
+        print("Leave Deduction:", Deduction)
+        print("Received Salary:", Receive_salary)
+                
         with open("salary_data.txt", "a") as file:
 
             file.write("\n----- Salary Record -----\n")
-            file.write(f"Name: {name}\n")
-            file.write(f"phone: {phone}\n")
-            file.write(f"monthly salary: {total_salary}\n")
-            file.write(f"Total Attendance: {attendance}\n")
-            file.write(f"Leave Deduction: {deduction}\n")
-            file.write(f"Received Salary: {receive_salary}\n")
+            file.write(f"Name: {Name}\n")
+            file.write(f"phone: {Phone}\n")
+            file.write(f"monthly salary: {Total_salary}\n")
+            file.write(f"Total Attendance: {Attendance}\n")
+            file.write(f"Leave Deduction: {Deduction}\n")
+            file.write(f"Received Salary: {Receive_salary}\n")
 
             print("\nData saved successfully in salary_data")
 
